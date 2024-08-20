d182472f8410c2a7f3079addaaa82923118e4e67
Clase1 <max.cirineo@tecsup.edu.pe>
2024-08-20 18:40:55 -0500

    Agregar archivos de proyecto.

---
+++ LabMax/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabMax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese las coordenadas del rectángulo:");
+            Console.Write("X1: ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Y1: ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("X2: ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Y2: ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+           
+            Rectangulo rectangulo = new Rectangulo(x1, y1, x2, y2, x3, y3, x4, y4);
+
+            
+            Console.WriteLine($"Base del rectángulo: {rectangulo.CalcularBase()}");
+            Console.WriteLine($"Altura del rectángulo: {rectangulo.CalcularAltura()}");
+            Console.WriteLine($"Área del rectángulo: {rectangulo.CalcularArea()}");
+            Console.WriteLine($"Perímetro del rectángulo: {rectangulo.CalcularPerimetro()}");
+        }
+    }
+}
---
+++ LabMax/Rectangulo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabMax
+{
+    internal class Rectangulo
+    {
+        public int X1 { get; set; }
+        public int Y1 { get; set; }
+        public int X2 { get; set; }
+        public int Y2 { get; set; }
+
+        public int X3 { get; set; }
+        public int Y3 { get; set; }
+
+        public int X4 { get; set; }
+        public int Y4 { get; set; }
+
+        public Rectangulo(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
+        {
+            X1 = x1;
+            Y1 = y1;
+            X2 = x2;
+            Y2 = y2;
+            X3 = x3;
+            Y3 = y3;
+            X4 = x4;
+            Y4 = y4;
+
+        }
+
+        
+        public int CalcularBase()
+        {
+            return Math.Abs(X2 - X1);
+        }
+
+       
+        public int CalcularAltura()
+        {
+            return Math.Abs(Y2 - Y1);
+        }
+
+       
+        public int CalcularArea()
+        {
+            return CalcularBase() * CalcularAltura();
+        }
+
+        
+        public int CalcularPerimetro()
+        {
+            return 2 * (CalcularBase() + CalcularAltura());
+        }
+    }
+}
